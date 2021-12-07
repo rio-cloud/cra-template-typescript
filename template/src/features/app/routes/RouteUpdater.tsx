fa2b611225c1756449eac3aa692e690e2953ff0c
Robin Kipsch <robin.kipsch@rio.cloud>
2021-12-07 09:25:26 +0100

    (feature) add route hooks for parsing and updating URL state

--- template/src/features/app/routes/RouteUpdater.tsx
+++ template/src/features/app/routes/RouteUpdater.tsx
@@ -1,13 +1,47 @@
-import { useEffect } from 'react';
+import { batch } from 'react-redux';
+import type { AnyAction } from 'redux';
 import { useLocation } from 'react-router-dom';
 
-const RouteUpdater = () => {
-    const location = useLocation();
+import { makeRoute, parseRoute } from './routes';
+import { useRouteState, useUpdateRoute } from './routeHooks';
+import { useAppDispatch, useAppSelector } from '../../../configuration/setup/hooks';
+// import { getSelectedMyId, getSelectedAnotherId, mySomethingSelected, anotherThingSelected } from '../appSlice';
 
-    useEffect(() => {
-        // Implement deep linking or react to window location change here
-        // console.log(location);
-    }, [location]);
+const RouteUpdater = () => {
+    const dispatch = useAppDispatch();
+    const { search } = useLocation();
+
+    // const selectedMyId = useAppSelector(getSelectedMyId);
+    // const selectedAnotherId = useAppSelector(getSelectedAnotherId);
+
+    // Parse initial route or after it has changed by browser navigation or user input
+    useRouteState(() => {
+        const dispatchQue: AnyAction[] = [];
+
+        const routeSearchParams = parseRoute(search);
+        // const { myId, anotherId } = routeSearchParams;
+
+        // if (myId !== selectedMyId) {
+        //     dispatchQue.push(mySomethingSelected(myId));
+        // }
+
+        // if (anotherId !== selectedAnotherId) {
+        //     dispatchQue.push(anotherThingSelected(anotherId));
+        // }
+
+        // batch(() => {
+        //     dispatchQue.forEach((action: AnyAction) => dispatch(action));
+        // });
+    });
+
+    // Update route whenever an observed store prop changes.
+    // The key of the RouteState is the search param key in the URL.
+    // const newRoute = makeRoute({
+    //     myId: selectedMyId,
+    //     anotherId: selectedAnotherId,
+    // });
+
+    // useUpdateRoute(newRoute);
 
     return null;
 };
